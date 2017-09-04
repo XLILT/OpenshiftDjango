3e2d2aa997a459f9aa6bb26228db5832163f3d52
XLILT <xiaolongma002@163.com>
2017-09-04 19:33:07 +0800

    django setting

--- welcome/views.py
+++ welcome/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. You're at the welcome index.")
+    #return HttpResponse("Hello World. You're at the welcome index.")
     #return HttpResponse(request.META['REMOTE_HOST'])
-    #context = {}
-    #context['hello'] = 'Hello World'
-    #return render(request, 'index.html', context)
+    context = {}
+    context['hello'] = 'Hello World'
+    return render(request, 'index.html', context)
 
