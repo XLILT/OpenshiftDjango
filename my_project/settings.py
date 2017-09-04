ea5c0e23a7e6f70963608f5252ed684f7fdff39a
XLILT <xiaolongma002@163.com>
2017-09-04 17:30:29 +0800

    django setting

--- my_project/settings.py
+++ my_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'common_static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
