8eaed449e9b09bfd2afde8f81d246f46c9671081
XLILT <xiaolongma002@163.com>
2017-09-04 16:43:19 +0800

    click interact

--- common_static/js/index.js
+++ common_static/js/index.js
@@ -1,3 +1,17 @@
+$(".nav.navbar-nav li").click(function() {
+	if($(this).text() === "注册") {
+		$(".form-registry").removeClass("hide");
+		$(".form-signin").addClass("hide");
+		$(".nav.navbar-nav li").removeClass("active");
+		$(this).addClass("active");
+	}
+	else if($(this).text() === "登陆") {
+		$(".form-signin").removeClass("hide");
+		$(".form-registry").addClass("hide");
+		$(".nav.navbar-nav li").removeClass("active");
+		$(this).addClass("active");
+	}
+});
+
 !function() {
-	console.log("xxx");
-}()
+}();
